2f0016a686e36e0e0f4f03c437cf51ecabbc1517
ABattleCat <168158419+ABattleCat@users.noreply.github.com>
2025-10-17 17:45:27 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        Leaderboard L = new Leaderboard();
+        L.addPlayer("Bob");
+        L.addPlayer("Bobby");
+        L.addPlayer("Bobbobby");
+        System.out.println(L.toString());
+        L.boardChange("Bobby", 100);
+        System.out.println(L.toString());
+    }
+
+}
